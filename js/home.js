7bd563f1c14d72405fe1ca96e2d0f7c22779b080
mariacristinatovarsalas <mariacristina.tovarsalas@gmail.com>
2020-02-12 00:30:40 -0300

    Subida final

--- js/home.js
+++ js/home.js
@@ -1,163 +1,94 @@
-
-/* GLOBAL VARIABLES*/
+/** VARIABLES */
 
 const apiKey = 'R6HlZmoZACiHenAqEf5l0jeXFDt2zUDG'
+
+/* Global */
 var url
 var gifContainer
 var suggestedTerm
-var historial = []
+var searchHistory = []
 
 
+/** GET SEARCH RESULTS FUNCTION 
+Executes the gif search when pressing the search button */
 
-/* BACK TO HOME */
-
-/* Every element that executes this function with a click allows 
-you to get back to the homepage */
-
-function backToHome() {
-  location.replace("home.html")
-}
-
-
-
-/* REDIRECT TO CREATE GIFS */
-
-/* Takes you to the page where you can create your own gifs */
-
-function redirectToCreateGifs() {
-  location.replace("./upload.html")
-}
-
-
-
-/* REDIRECT TO "MIS GUIFOS" */
-
-/* Takes you to the page where the created gifs are saved */
-
-function redirectToMisGuifos() {
-  location.replace("./mis_guifos.html")
-}
-
-
-
-/*THEMES DROPDOWN*/
-
-function dropdown() {
-  document.getElementById("dropdownContent").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.pinkButtons')) {
-    var dropdowns = document.getElementsByClassName("dropdownContent");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
-
-
-/* THEME SELECTION */
-
-/* When the website loads, this function access the Local Storage to identify the 
-existence of a key 'theme' with a value of 'light' or 'dark'. Depending on the existing value,
-it executes a function or another */
-
-function themeSelection() {
-  if (localStorage.getItem('theme') === 'dark') {
-    changeToDarkTheme()
-  } else {
-    changeToLightTheme()
-  }
-}
-
-function changeToDarkTheme() {
-  localStorage.setItem('theme', 'dark')
-  document.getElementById("light").setAttribute("href", "styles/sailor_night.css")
-  document.getElementById("logo").setAttribute("src", "images/gifos_logo_dark.png")
-  document.getElementById("lens").setAttribute("src", "images/lens_inactive_dark.svg")
-}
-
-function changeToLightTheme() {
-  localStorage.setItem('theme', 'light')
-  document.getElementById("light").setAttribute("href", "styles/sailor_day.css")
-  document.getElementById("logo").setAttribute("src", "images/gifos_logo_light.png")
-  document.getElementById("lens").setAttribute("src", "images/lens_inactive_light.svg")
-}
-
-
-
-
-
-/* GET SEARCH RESULTS FUNCTION */
-
-/* Executes the gif search when pressing the search button */
-
-function getSearchResults(Term) {
+function getSearchResults(term) {
+  /* Displays an arrow button that allows the user to go back to the homepage */
+  document.getElementById("arrowBackHome").style.display = "block";
   let searchedTerm
   /* Empties the gif container */
   document.getElementById(gifContainer).innerHTML = "";
+  /* When the function is executed across the code, the conditional evaluates if there's a parameter
+  that's been included, if it is the case, it's going to represent the value of the 
+  searchedTerm. */
 
-  if (Term) {
-    searchedTerm = Term
+  if (term) {
+    searchedTerm = term
   }
   else {
-    /* The searched term is introduced in the search history array
-    and saved in Local Storage as a string */
     searchedTerm = document.getElementById("searchBar").value
-    /* If the function is executed with an empty string, it returns nothing */
-    if (searchedTerm == "") { return } 
-    historial.push(searchedTerm)
-    localStorage.setItem("historial", historial.join(","))
+    /* If the function is executed with an empty string, it stops the evaluation
+    and exits the function */
+    if (searchedTerm == "") { return }
+    /* The 'some' method evaluates array elements looking for a coincidence with the
+    previous set condition. It evaluates the comparison beetween the searchedTerm 
+    and the repeatedTerm, if there isn't one, it sets the term in Local Storage. 
+    This to avoid repeating the presence of the term in Local Storage. */
+    if (!searchHistory.some(function (repeatedTerm) { return repeatedTerm === searchedTerm })) {
+      searchHistory.push(searchedTerm)
+      localStorage.setItem("searchHistory", searchHistory.join(","))
+    }
   }
 
-  if (searchedTerm !== "") {
-    /* If search term is different from an empty string, the server request function
+  /* If search term is different from an empty string, the server request function
     is executed with the following parameters */
-    url = 'http://api.giphy.com/v1/gifs/search?q=' + searchedTerm + '&api_key=' + apiKey + '&limit=24'
+  if (searchedTerm !== "") {
+    url = 'https://api.giphy.com/v1/gifs/search?q=' + searchedTerm + '&api_key=' + apiKey + '&limit=24'
     gifContainer = 'searchResultsContainer'
     serverRequest(url, gifContainer, true)
-    // CAMBIA CON EL SCROLL DOWN
     document.getElementById('suggestionsSection').style.display = "none";
     document.getElementById('trendingsSection').style.display = "none";
     document.getElementById('searchResultsContainer').style.display = "flex";
 
-    //COLOCAR EXPLICACION AQUÍ
-    var presentGifInLocalStorage = false
+
+    /* Every term that the user searches for is going to be saved
+    in Local Storage. When the search reaches the 5th term, the first
+    four terms saved in Local Storage are going to be replaced randomly
+    by the most recent searches. The value of the key is changed with
+    this random number assigned by the "math.random" function */
+    var exitWhile = false
     var i = 1
-    while (presentGifInLocalStorage == false) {
+    while (exitWhile == false) {
       if (localStorage.getItem("gifo" + i) == null) {
         localStorage.setItem("gifo" + i, JSON.stringify(searchedTerm))
-        presentGifInLocalStorage = true
+        exitWhile = true 
       } else if (i == 4) {
         localStorage.setItem("gifo" + (Math.floor(Math.random() * 4) + 1), JSON.stringify(searchedTerm))
-        presentGifInLocalStorage = true
+        exitWhile = true
       }
       else {
         i++
       }
     }
   }
+  //Clean divSuggestedSearchTerms
+  document.getElementById("searchBar").value=""
+  displaySuggestedSearchTerms()
 }
 
 
-/* DISPLAY BLUE BUTTONS */
-
-/* Show searched terms displayed in blue buttons under the search section */
+/** DISPLAY BLUE BUTTONS
+Shows searched terms displayed in blue buttons under the search section */
 
 function displayBlueButtons() {
   var blueButtonsContainer = document.getElementById("blueButtonsContainer")
-  /* Turns the search history array into a string */
-  historial = localStorage.getItem("historial").split(",")
-
-  /* Use reverse method to show the last search at the search suggestions container's top left*/
-  historial.reverse().forEach(function(previousSearch) {
-    /* When there's an iteration of the previous search array it prints the terms
+  /* Empties the container to avoid repeating the buttons printing several times */
+  blueButtonsContainer.innerHTML = ""
+  /* Turns the search history string into an array */
+  searchHistory = localStorage.getItem("searchHistory").split(",")
+  /* Use reverse method to show the last search at the search suggestions container's top left */
+  searchHistory.reverse().forEach(function (previousSearch) {
+    /* When there's an iteration of the search history array it prints the terms
     as blue buttons in the screen */
     var blueButton = document.createElement("BUTTON")
     blueButton.setAttribute('class', 'blueButton')
@@ -171,10 +102,9 @@ displayBlueButtons()
 
 
 
-/* DISPLAY SUGGESTED SEARCH TERMS */
- 
-/* This function displays a div containing search suggestions for the user based on previous searchs (dynamic search). 
-It's executed with every "KeyUp" event in the search bar */
+/** DISPLAY SUGGESTED SEARCH TERMS
+This function displays a div containing search suggestions for the user based on 
+previous searchs (dynamic search). It's executed with every "KeyUp" event in the search bar */
 
 function displaySuggestedSearchTerms() {
   /* Identifies the current value of the search bar after every "KeyUp" event */
@@ -183,41 +113,43 @@ function displaySuggestedSearchTerms() {
   var divSearchSuggestions = document.getElementById("divSearchSuggestions")
   /* Refreshes div content for every "KeyUp" event */
   divSearchSuggestions.innerHTML = ""
-
   /* Evaluates if search bar is empty to hide dynamic search div */
   if (searchBar == "") {
     divSearchSuggestions.style.display = "none";
-    /* Changes lens icon style if there's no active search */
-    document.getElementById("lens").setAttribute('src', './images/lens_inactive_light.svg')
+    /* Changes lens icon and search button style if there's no active search */
+    document.getElementById("lens").setAttribute('src', './images/lens_inactive_'+theme+'.svg')
+    document.getElementById("searchButton").setAttribute('class', 'searchButton')
   }
 
-  /* If there is content in the search bar displays dynamic search div */  
+  /* If there is content in the search bar the dynamic search div is displayed */
   else {
-    divSearchSuggestions.style.display = "flex";
-    /* Changes lens icon style if there's an active search and changes search button style */
-    document.getElementById("lens").setAttribute('src', './images/lens_active_light.svg')
-    document.getElementById("searchButton").setAttribute('class', 'searchButtonDarkActive')
-    /* Filters search history terms array if the current search term
-    contains the character combination present in the dynamic search bar. 
-    The "filteredTerms" variable returns and array with the filtered terms
-    (the ones that had a match) */
-    var filteredTerms = historial.filter(function(searchTerm) {
-    return searchTerm.includes(searchBar)
+    /* Changes lens icon and search button style if there's an active search */
+    document.getElementById("lens").setAttribute('src', './images/lens_active_'+theme+'.svg')
+    document.getElementById("searchButton").setAttribute('class', 'searchButtonDarkActive pinkButtons')
+    /* Filters search history array terms assuming as condition the present 
+    string in search bar. The "filteredTerms" variable returns and array with 
+    the filtered terms (the ones that had a match) */
+    var filteredTerms = searchHistory.filter(function (searchTerm) {
+      divSearchSuggestions.style.display = "none";
+      return searchTerm.includes(searchBar)
     })
 
     /* It iterates on filter results to print search terms in dynamic search div */
     for (let i = 0; i < 3; i++) {
-      /* It assigns as a value for the term variable the position value of the filters terms array */
+      /* It assigns as a value for the term variable the position value of the 
+      filters terms array */
       var matchTerm = filteredTerms[i]
       /* Evaluates if filtered terms array is empty and if is not the case, it prints 
       the term in the dynamic search div */
+
       if (matchTerm != undefined) {
+        divSearchSuggestions.style.display = "flex";
         matchTermDiv = document.createElement("DIV")
         matchTermDiv.setAttribute("class", "grayButtons")
         matchTermDiv.innerHTML = matchTerm
         matchTermDiv.setAttribute("onclick", "getSearchResults('" + matchTerm + "')")
         divSearchSuggestions.appendChild(matchTermDiv)
-      }
+      } 
     }
   }
 
@@ -225,45 +157,43 @@ function displaySuggestedSearchTerms() {
 
 
 
-/* SHOW SEARCH SUGGESTIONS */
-
-/* Converts the search history located in Local Storage (presented as a string) into an array */
-
-if (localStorage.getItem("historial")) {
-  historial = localStorage.getItem("historial").split(",")
+/** SHOW SEARCH SUGGESTIONS
+Converts the search history located in Local Storage (presented as a string) into an array */
+if (localStorage.getItem("searchHistory")) {
+  searchHistory = localStorage.getItem("searchHistory").split(",")
 }
 
 
 
 
-/*SUGGESTIONS*/
-
-/* It executes when the site loads. If the users hasn't made any searchs, it prints random
-gifs in screen to mantain the original desing of the page */
+/** LOAD SUGGESTIONS
+It executes when the site loads and the user deletes a suggestion term.
+If the user hasn't made any searches, it prints random
+gifs in screen to mantain the original page design */
 
 function loadSuggestions() {
-
+document.getElementById("suggestionsContainer").innerHTML=""
   for (let i = 1; i <= 4; i++) {
-    suggestedTerm = localStorage.getItem("gifo" + i)
+    let nameSavedGifo = "gifo" + i
+    suggestedTerm = localStorage.getItem(nameSavedGifo)
     if (suggestedTerm != null) {
-      url = 'http://api.giphy.com/v1/gifs/search?q=' + suggestedTerm + '&api_key=' + apiKey + '&limit=1'
+      url = 'https://api.giphy.com/v1/gifs/search?q=' + suggestedTerm + '&api_key=' + apiKey + '&limit=1'
     }
     else {
-      url = 'http://api.giphy.com/v1/gifs/random?api_key=' + apiKey
+      url = 'https://api.giphy.com/v1/gifs/random?api_key=' + apiKey
     }
     gifContainer = 'suggestionsContainer'
-    serverRequest(url, gifContainer, false, suggestedTerm);
+    serverRequest(url, gifContainer, false, nameSavedGifo);
 
   }
 }
 
 
 
-/*TRENDINGS*/
-
-/* It executes when the page loads printing trending gifs in screen */
+/** LOAD TRENDINGS
+It executes when the page loads printing trending gifs in screen */
 function loadTrendings() {
-  url = 'http://api.giphy.com/v1/gifs/trending?api_key=' + apiKey + '&limit=24'
+  url = 'https://api.giphy.com/v1/gifs/trending?api_key=' + apiKey + '&limit=24'
   gifContainer = 'trendingsContainer'
   serverRequest(url, gifContainer, true)
 }
@@ -271,12 +201,11 @@ function loadTrendings() {
 
 
 
-
 /* NECESSARY FUNCTIONS IN EVERY GIF DISPLAY */
 
-/*SERVER REQUEST*/
+/** SERVER REQUEST*/
 
-function serverRequest(url, gifContainer, classicShowData, smallTrendingContainer) {
+function serverRequest(url, gifContainer, classicShowData, nameSavedGifo) {
   const found =
     fetch(url)
       .then(function (response) {
@@ -284,7 +213,7 @@ function serverRequest(url, gifContainer, classicShowData, smallTrendingContaine
       })
       .then(function (data) {
         console.log(data)
-        showData(data, gifContainer, classicShowData, smallTrendingContainer)
+        showData(data, gifContainer, classicShowData, nameSavedGifo)
         return data
       })
       .catch(function (error) {
@@ -298,11 +227,16 @@ function serverRequest(url, gifContainer, classicShowData, smallTrendingContaine
 
 
 
-/* SHOW DATA FUNCTION */
+/** SHOW DATA FUNCTION 
+Prints every gif in screen */
 
-/* Prints every gif in screen ("classic" it's a reference for all the non suggestions gif) */
+/* Parameters: 1) data: every gif data provided by the server
+               2) gifContainer: div that should contain the specific gifs
+               3) classicShowData: it's a reference for all the non suggestions gifs
+               4) nameSavedGifo: specific reference about every suggestion gif present in 
+                  Local Storage. */
 
-function showData(data, gifContainer, classicShowData) {
+function showData(data, gifContainer, classicShowData, nameSavedGifo) {
 
   /* Identifies if the data is an object array or a single object 
   (to decide if there's need to iterate or not the painting gif function) */
@@ -321,13 +255,13 @@ function showData(data, gifContainer, classicShowData) {
 
     var createdImageToContainGif = document.createElement("IMG")
     createdImageToContainGif.setAttribute('src', gif.images.original.url)
+    createdImageToContainGif.setAttribute('onclick',"openInGhipy('" + gif.images.original.url + "')")
     var trendingGifContainer = document.createElement("DIV")
     trendingGifContainer.appendChild(createdImageToContainGif)
 
     /* Evaluates if the gif is "classic" and sets its own attributes 
     ("classic" refers to every gif printed in the site except for the suggestion gifs) */
-  
-
+    
     if (classicShowData === true) {
       createdImageToContainGif.setAttribute('class', 'trendingGifs')
 
@@ -346,12 +280,11 @@ function showData(data, gifContainer, classicShowData) {
 
       /* If the gif's not classic it prints as a suggestion gif and its own attributes 
       are setted */
-
     } else {
       createdImageToContainGif.setAttribute('class', 'suggestionGif')
 
       /* Suggestion gifs title attributes */
-    
+
       var gifTitle = gif.title;
       var gifTitleArray = gifTitle.split(" ")
       var gifTitleArrayUpperCase = gifTitleArray.map(function (title) {
@@ -379,11 +312,10 @@ function showData(data, gifContainer, classicShowData) {
 
       var closeButton = document.createElement("BUTTON")
       closeButton.setAttribute('class', 'closeButton')
-      closeButton.onclick = function removeSuggestionGif() {
-        localStorage.removeItem("gifo" + i, suggestedTerm);
-      }
-
-      /* Dymanic elements of each suggestion gif */ 
+      closeButton.setAttribute('onclick', "removeSuggestionGif('" + nameSavedGifo +"')")
+  
+    
+      /* Dynamic elements of each suggestion gif */
 
       var suggestionGifContainer = document.createElement("DIV")
       suggestionGifContainer.insertBefore(suggestionGifBar, suggestionGifContainer.firstChild)
@@ -394,4 +326,15 @@ function showData(data, gifContainer, classicShowData) {
       document.getElementById(gifContainer).appendChild(suggestionGifContainer)
     }
   }
+}
+
+/** REMOVE SUGGESTION GIF */
+function removeSuggestionGif(deletedSuggestionGif) {
+  localStorage.removeItem(deletedSuggestionGif);
+  loadSuggestions()
+}
+
+/** OPEN IN GHIPY */
+function openInGhipy(link) {
+  window.open(link);
 }
